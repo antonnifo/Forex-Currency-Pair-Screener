f1f5da3dbb85243bff690f6606f01c50cc078f53
antonnifo <antonnifo@live.com>
2023-06-02 18:43:21 +0300

    limit currency choices

--- currency_chart_app/models.py
+++ currency_chart_app/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 class CurrencyData(models.Model):
+    CURRENCY_CHOICES = (
+                    ('GBPUSD', 'GBPUSD'),
+                    ('EURAUD', 'EURAUD'),
+                    ('USDCAD', 'USDCAD'),
+                )    
+
     timestamp = models.DateTimeField()
     open = models.FloatField()
     high = models.FloatField()
     low = models.FloatField()
     close = models.FloatField()
-    currency_code = models.CharField(max_length=10)
+    currency_code = models.CharField(max_length=10,choices=CURRENCY_CHOICES, default='USDCAD')
+
+    def __str__(self):
+        return self.currency_code
+    
 
