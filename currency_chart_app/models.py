c0309b74b93b12044e59b20c55b32191904fd779
antonnifo <antonnifo@live.com>
2023-06-02 16:27:20 +0300

    add CurrencyData model

--- currency_chart_app/models.py
+++ currency_chart_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class CurrencyData(models.Model):
+    timestamp = models.DateTimeField()
+    open = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    currency_code = models.CharField(max_length=10)
+
