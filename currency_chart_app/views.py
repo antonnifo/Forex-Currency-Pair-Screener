92f56f4373bed1c3586ddeee714f7c8ffe8630dd
antonnifo <antonnifo@live.com>
2023-06-02 16:31:07 +0300

    add view to render currency data

--- currency_chart_app/views.py
+++ currency_chart_app/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+import mplfinance as mpf
+import pandas as pd
+from .models import CurrencyData
 
-def index(request):
-    pass
+def candlestick_chart(request):
+    # if request.method == 'POST': 
+    currency_data = CurrencyData.objects.filter(currency_code='USD') 
+    
+    data = pd.DataFrame(list(currency_data.values()))
+
+    data['timestamp'] = pd.to_datetime(data['timestamp'])
+    data.set_index('timestamp', inplace=True)
+
+    context = {'data': data.to_json(orient='records')}
+    
+    return render(request, 'currency_chart/candlestick_chart.html', context)
