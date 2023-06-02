29621b5f7aaf22b39f1f487032df5e0bb7082056
antonnifo <antonnifo@live.com>
2023-06-02 16:26:55 +0300

    register currencyData model

--- currency_chart_app/admin.py
+++ currency_chart_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CurrencyData
 
-# Register your models here.
+@admin.register(CurrencyData)
+class CurrencyDataAdmin(admin.ModelAdmin):
+    list_display = ['timestamp', 'open', 'high',
+                    'low', 'close', 'currency_code']
+    list_filter = ['currency_code', 'timestamp']
+
