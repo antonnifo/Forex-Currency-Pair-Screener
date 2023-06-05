97f4de9581bdd031de31ac6511db30007b69b1ba
antonnifo <antonnifo@live.com>
2023-06-05 14:12:59 +0300

    use ImportExportModelAdmin

--- currency_chart_app/admin.py
+++ currency_chart_app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
+
 from .models import CurrencyData
+from import_export.admin import ImportExportModelAdmin
+
+
+
 
 @admin.register(CurrencyData)
-class CurrencyDataAdmin(admin.ModelAdmin):
+class CurrencyDataAdmin(ImportExportModelAdmin):
     list_display = ['timestamp', 'open', 'high',
                     'low', 'close', 'currency_code']
     list_filter = ['currency_code', 'timestamp']
