cb0c788fbd19ae9a4cb1d069adc39816b038076f
antonnifo <antonnifo@live.com>
2023-06-11 16:03:33 +0300

    register new model

--- currency_chart_app/admin.py
+++ currency_chart_app/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 
-from .models import CurrencyData
+from .models import CurrencyData, Interval
 from import_export.admin import ImportExportModelAdmin
 
-
+@admin.register(Interval)
+class IntervalDataAdmin(ImportExportModelAdmin):
+    list_display = ['name',]
+    list_filter = ['name',]
 
 
 @admin.register(CurrencyData)
 class CurrencyDataAdmin(ImportExportModelAdmin):
-    list_display = ['timestamp', 'open', 'high',
+    list_display = ['timestamp', 'interval', 'open', 'high',
                     'low', 'close', 'currency_code']
     list_filter = ['currency_code', 'timestamp']
 
