7532a544f2c7f82431d4e7dc07a6ada220335e76
antonnifo <antonnifo@live.com>
2023-06-12 11:40:32 +0300

    use list instead of dataframe 4data object

--- currency_chart_app/views.py
+++ currency_chart_app/views.py
@@ -3,23 +3,26 @@ import mplfinance as mpf
 import pandas as pd
 from .models import CurrencyData
 
+
 def candlestick_chart(request, currency=None):
 
+    currency_data = CurrencyData.objects.filter(currency_code='USDCAD') 
     if currency:
-        try:
-            currency_data = CurrencyData.objects.filter(currency_code=currency)
-        except Exception as e:
-            raise Http404('No Currency Data for that Currency Code, Check back later')
-    else:
-        currency_data = CurrencyData.objects.filter(currency_code='USDCAD')    
+        currency_data = CurrencyData.objects.filter(currency_code=currency) 
     
-    print(currency_data)
-    data = pd.DataFrame(list(currency_data.values()))
-
-    data['timestamp'] = pd.to_datetime(data['timestamp'])
-    data.set_index('timestamp', inplace=True)
-
-    context = {'data': data.to_json(orient='records'),
-              'currency':currency}
+    data = []
     
+    for entry in currency_data:
+        data.append({
+            'timestamp': entry.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
+            'open': entry.open,
+            'high': entry.high,
+            'low': entry.low,
+            'close': entry.close,
+        })
+    
+    context = {'data': data,
+               'currency':currency}
+
     return render(request, 'currency_chart/candlestick_chart.html', context)
+    
